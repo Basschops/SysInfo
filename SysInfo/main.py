5ee3a811549150b3e58564afe4fcca57bacc590c
Daragh O'Farrell <daraghofarrell@gmail.com>
2018-02-06 09:45:40 +0000

    My first push

---
+++ SysInfo/main.py
@@ -0,0 +1,12 @@
+'''
+Module to display system info
+'''
+
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ diskS.py
@@ -0,0 +1,4 @@
+ds = os.stats()
+
+print(ds)
+
